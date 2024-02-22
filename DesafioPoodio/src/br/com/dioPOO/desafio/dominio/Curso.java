3a4869b8616c6836a4410ae2a09dcfe3c6ae2d81
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-02-22 18:07:38 -0300

    dando continuidade no desafio

---
+++ DesafioPoodio/src/br/com/dioPOO/desafio/dominio/Curso.java
@@ -0,0 +1,8 @@
+package br.com.dioPOO.desafio.dominio;
+
+public class Curso {
+	String titulo;
+	String descrição;
+	int cargahoraria;
+
+}
---
+++ DesafioPoodio/src/br/com/dioPOO/desafio/dominio/Mentoria.java
@@ -0,0 +1,10 @@
+package br.com.dioPOO.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria {
+	String titulo;
+	String descricao;
+	LocalDate data; // Permite trabalhar com datas!
+
+}
