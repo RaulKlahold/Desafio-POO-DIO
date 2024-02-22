38f24bb15ff5a9d6751a2d1399cc94d495783d1e
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-02-22 18:11:05 -0300

    dando continuidade

--- DesafioPoodio/src/br/com/dioPOO/desafio/dominio/Mentoria.java
+++ DesafioPoodio/src/br/com/dioPOO/desafio/dominio/Mentoria.java
@@ -3,8 +3,26 @@ package br.com.dioPOO.desafio.dominio;
 import java.time.LocalDate;
 
 public class Mentoria {
-	String titulo;
-	String descricao;
-	LocalDate data; // Permite trabalhar com datas!
+	private String titulo;
+	private String descricao;
+	private LocalDate data; // Permite trabalhar com datas!
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
 
 }
