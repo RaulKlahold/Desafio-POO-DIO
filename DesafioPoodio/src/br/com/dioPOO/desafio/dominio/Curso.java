8d36ff3a1caab328bf0c5139f1875caedb228b1e
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-02-22 18:10:28 -0300

    dandco continuidade no desafio

--- DesafioPoodio/src/br/com/dioPOO/desafio/dominio/Curso.java
+++ DesafioPoodio/src/br/com/dioPOO/desafio/dominio/Curso.java
@@ -1,8 +1,26 @@
 package br.com.dioPOO.desafio.dominio;
 
 public class Curso {
-	String titulo;
-	String descrição;
-	int cargahoraria;
+	private String titulo;
+	private String descrição;
+	private int cargahoraria;
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescrição() {
+		return descrição;
+	}
+	public void setDescrição(String descrição) {
+		this.descrição = descrição;
+	}
+	public int getCargahoraria() {
+		return cargahoraria;
+	}
+	public void setCargahoraria(int cargahoraria) {
+		this.cargahoraria = cargahoraria;
+	}
 
 }
